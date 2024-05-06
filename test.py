d0422d85c612638ce6869b30f2ddd1978162e8ae
Takumi Hoshino <takuhossy0901@gmail.com>
2024-05-06 23:11:57 +0900

    Update two files.

--- test.py
+++ test.py
@@ -1,10 +1,1 @@
-import scipy.optimize as op
-import numpy.random as npr
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import sympy as sy
-import math
-
-
 print("Hello World")
